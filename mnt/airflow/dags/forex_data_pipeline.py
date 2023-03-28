4c81c2556866ec0b986da7bd916ab662aab5cd75
EC2 Default User <ec2-user@ip-172-31-46-249.eu-west-3.compute.internal>
2023-03-28 19:48:18 +0000

    Add http sensor forex data pipeline dag

--- mnt/airflow/dags/forex_data_pipeline.py
+++ mnt/airflow/dags/forex_data_pipeline.py
@@ -1,5 +1,6 @@
 
 from airflow import DAG
+from airflow.providers.http.sensors.http import HttpSensor
 from datetime import datetime, timedelta
 
 default_args = {
@@ -16,5 +17,14 @@ with DAG(
   "forex_data_pipeline"
   , start_date = datetime(2023, 3, 26)
   , schedule_interval = "@daily"
-  , default_args = default_args) as dag:
-  pass
+  , default_args = default_args
+  , catchup=False) as dag:
+
+  is_forex_rates_available = HttpSensor(
+    task_id = "is_forex_rates_available", # must be unique within the same dag
+    http_conn_id = "forex_api",
+    endpoint = "marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b",
+    response_check = lambda response: "rates" in response.text,
+    poke_interval = 5,
+    timeout = 20 # after 20 seconds - if no response, it will fail
+  )
