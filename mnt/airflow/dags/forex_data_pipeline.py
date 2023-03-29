73769428b0efff95ff71e8a40c06614901ee55ab
EC2 Default User <ec2-user@ip-172-31-46-249.eu-west-3.compute.internal>
2023-03-29 21:56:33 +0000

    Add python and bash operator

--- mnt/airflow/dags/forex_data_pipeline.py
+++ mnt/airflow/dags/forex_data_pipeline.py
@@ -1,7 +1,13 @@
 
 from airflow import DAG
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.sensors.filesystem import FileSensor
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
+import requests
+import csv
+import json
 
 default_args = {
   "owner": "airflow",
@@ -11,6 +17,25 @@ default_args = {
   "retries": 1,
   "retry_delay": timedelta(minutes=3)
 }
+
+def download_rates():
+  BASE_URL = "https://gist.githubusercontent.com/marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b/raw/"
+  ENDPOINTS = {
+      'USD': 'api_forex_exchange_usd.json',
+      'EUR': 'api_forex_exchange_eur.json'
+  }
+  with open('/opt/airflow/dags/files/forex_currencies.csv') as forex_currencies:
+    reader = csv.DictReader(forex_currencies, delimiter=';')
+    for idx, row in enumerate(reader):
+      base = row['base']
+      with_pairs = row['with_pairs'].split(' ')
+      indata = requests.get(f"{BASE_URL}{ENDPOINTS[base]}").json()
+      outdata = {'base': base, 'rates': {}, 'last_update': indata['date']}
+      for pair in with_pairs:
+          outdata['rates'][pair] = indata['rates'][pair]
+      with open('/opt/airflow/dags/files/forex_rates.json', 'a') as outfile:
+          json.dump(outdata, outfile)
+          outfile.write('\n')
 
 with DAG(
   # id must be unique across the airflow instance
@@ -27,4 +52,24 @@ with DAG(
     response_check = lambda response: "rates" in response.text,
     poke_interval = 5,
     timeout = 20 # after 20 seconds - if no response, it will fail
+  )
+
+  is_forex_file_available = FileSensor(
+    task_id = "is_forex_file_available",
+    fs_conn_id = "forex_path",
+    filepath = "forex_currencies.csv",
+    poke_interval = 5,
+    timeout = 20
+  )
+
+  downloading_rates = PythonOperator(
+    task_id = "downloading_rates",
+    python_callable=download_rates
+  )
+
+  saving_rates = BashOperator(
+    task_id = "saving_rates",
+    bash_command="""
+      hdfs dfs -mkdir -p /forex && hdfs dfs -put -f $AIRFLOW_HOME/dags/files/forex_rates.json /forex
+    """
   )
