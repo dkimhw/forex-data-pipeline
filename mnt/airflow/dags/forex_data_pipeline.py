1db5a652f2ec6de77cba4e62d2def1f61ff1644c
EC2 Default User <ec2-user@ip-172-31-46-249.eu-west-3.compute.internal>
2023-04-02 21:50:46 +0000

    Add hive operator

--- mnt/airflow/dags/forex_data_pipeline.py
+++ mnt/airflow/dags/forex_data_pipeline.py
@@ -4,6 +4,7 @@ from airflow.providers.http.sensors.http import HttpSensor
 from airflow.sensors.filesystem import FileSensor
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
+# from airflow.providers.apache.hiver.operators.hive import HiveOperator
 from datetime import datetime, timedelta
 import requests
 import csv
@@ -73,3 +74,23 @@ with DAG(
       hdfs dfs -mkdir -p /forex && hdfs dfs -put -f $AIRFLOW_HOME/dags/files/forex_rates.json /forex
     """
   )
+
+  # creating_forex_rates_table = HiveOperator(
+  #   task_id="creating_forex_rates_table",
+  #   hive_cli_conn_id="hive_conn",
+  #   hql="""
+  #       CREATE EXTERNAL TABLE IF NOT EXISTS forex_rates(
+  #         base STRING,
+  #         last_update DATE,
+  #         eur DOUBLE,
+  #         usd DOUBLE,
+  #         nzd DOUBLE,
+  #         gbp DOUBLE,
+  #         jpy DOUBLE,
+  #         cad DOUBLE
+  #         )
+  #       ROW FORMAT DELIMITED
+  #       FIELDS TERMINATED BY ','
+  #       STORED AS TEXTFILE
+  #   """
+  # )
